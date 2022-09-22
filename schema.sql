02247b6f00b0f7362c4fdbfe18e216ec9af9fd4d
businesskaniki <businesskanikii@gmail.com>
2022-09-23 01:18:04 +0300

    droped species column and added two more columns

--- schema.sql
+++ schema.sql
@@ -7,5 +7,22 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL,
 );
-
 ALTER TABLE animals ADD COLUMN  species TEXT;
+
+CREATE TABLE owners (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name TEXT,
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species(
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id) ON DELETE CASCADE;
