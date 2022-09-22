9b3780e052bf1f10cd0ebe24a7a33b7432883250
businesskaniki <businesskanikii@gmail.com>
2022-09-22 06:54:41 +0300

    adding more queries to the db

--- data.sql
+++ data.sql
@@ -4,3 +4,10 @@ INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Gabumon',2,'15-11-2018',2,true,8);
 INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Pikachu',3,'7-1-2021',1,false,15.04);
 INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Devimon',4,'12-5-2017',5,true,11);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Charmander',5,'8-2-2020',0,false,-11);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Plantmon',6,'15-12-2021',2,true,-5.7);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Squirtle',7,'2-3-1993',3,false,-12.13);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Angemon',8,'12-6-2005',1,true,-45);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Boarmon',9,'7-6-2005',7,true,20.4);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Blossom',10,'13-10-1998',3,true,17);
+INSERT INTO animals (name,id,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Ditto',11,'14-5-2022',4,true,22);
